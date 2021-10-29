2f72466ccbc352a885456fa5728d08720e893a9a
Ranjit <ranjith.victor@hashconnect.in>
2021-10-29 06:17:02 +0530

    Menu based admin area

--- src/app/organization/orgadmin/orgadmin.component.ts
+++ src/app/organization/orgadmin/orgadmin.component.ts
@@ -1,3 +1,4 @@
+import { BehaviorSubject } from 'rxjs';
 import { OrgTag, TemplateLibrary } from './../../model/dahsboard';
 import { AdminService } from './../../admin.service';
 import { Role, User } from './../../model/user';
@@ -29,6 +30,8 @@ export class OrgadminComponent implements OnInit {
     users: User[];
     docs: TemplateLibrary[];
     tags: OrgTag[];
+    selectedMenu: BehaviorSubject<any> = new BehaviorSubject({ item: "roles", title: "Roles" });
+
     constructor(
         public appComp: AppComponent,
         private adminService: AdminService
@@ -36,9 +39,35 @@ export class OrgadminComponent implements OnInit {
 
     ngOnInit() {
         this.appComp.subMenu = { name: 'Organization Admin' };
+
+        this.selectedMenu.subscribe(val => {
+            if (val.item === 'roles') {
+                this.adminService.getOrgRoles(this.appComp.loggedInUser).then((data: Role[]) => {
+                    this.roles = data;
+                });
+            } else if (val.item === 'users') {
+                this.adminService.getOrgUsers(this.appComp.loggedInUser).then((data: User[]) => {
+                    this.users = data;
+                    this.adminService.getOrgRoles(this.appComp.loggedInUser).then((data: Role[]) => {
+                        this.roles = data;
+                    });
+                });
+            } else if (val.item === 'library') {
+                this.adminService.getLibraryDocs(this.appComp.loggedInUser).then((data: TemplateLibrary[]) => {
+                    this.docs = data;
+                });
+            } else if (val.item === 'tags') {
+                this.adminService.getOrgTags(this.appComp.loggedInUser).then((data: OrgTag[]) => {
+                    this.tags = data;
+                });
+            }
+        });
+
         this.adminService.getOrgRoles(this.appComp.loggedInUser).then((data: Role[]) => {
             this.roles = data;
         });
+
+        this.selectedMenu.next({ item: "roles", title: "Roles" });
     }
 
     tabSelected(ev: MatTabChangeEvent) {
@@ -56,13 +85,15 @@ export class OrgadminComponent implements OnInit {
         } else if (ev.tab.textLabel === 'Library') {
             this.adminService.getLibraryDocs(this.appComp.loggedInUser).then((data: TemplateLibrary[]) => {
                 this.docs = data;
-                //this.appComp.currentTenant.templateLibrary = this.docs;
             });
         } else if (ev.tab.textLabel === 'Tags') {
             this.adminService.getOrgTags(this.appComp.loggedInUser).then((data: OrgTag[]) => {
                 this.tags = data;
-                //this.appComp.currentTenant.templateLibrary = this.docs;
             });
         }
+    }
+
+    switchMenu(item, title) {
+        this.selectedMenu.next({ item: item, title: title });
     }
 }
