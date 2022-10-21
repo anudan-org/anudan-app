8cf491d4f95a4ab92cf1311b24c26fc999f4eea9
raghavendrakedlaya <kedlayaru@gmail.com>
2022-10-21 11:15:13 +0530

    toStateOwner.active - says does not exists.

--- src/app/closure/closure-preview/closure-preview.component.ts
+++ src/app/closure/closure-preview/closure-preview.component.ts
@@ -584,8 +584,10 @@ export class ClosurePreviewComponent implements OnInit {
       toState = this.currentClosure.flowAuthorities.filter(a => a.fromStateId === toStateId)[0].fromName;
     }
     const toStateOwner = this.currentClosure.workflowAssignment.filter(a => a.stateId === toStateId)[0].assignmentUser;
-    let username = (!toStateOwner.active ? ('Unregistered: ' + toStateOwner.emailId) : toStateOwner.firstName + ' ' + toStateOwner.lastName) ;
-    if ( toStateOwner.active ) {
+    let activeuser= toStateOwner.active;
+    let username = (!activeuser ? ('Unregistered: ' + toStateOwner.emailId) : toStateOwner.firstName + ' ' + toStateOwner.lastName) ;
+    
+    if ( activeuser) {
       return toStateOwner ? (toState + "<span class='text-subheader'> [ " + username + " ]</span>") : "";
     } else {
       return toStateOwner ? (toState + "<span class='text-subheader-red' > [ " + username + " ]</span>") : "";
